75f63fc68707976fd4b1a560553e85d9a56bbaab
Nilesh <nilshinde74@gmail.com>
2022-04-22 11:59:06 +0530

    adding user page there

--- Hello/isbm_system/home/models.py
+++ Hello/isbm_system/home/models.py
@@ -1,4 +1,21 @@
+from distutils.command.upload import upload
+import profile
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    USER =(
+        (1,'HOD'),
+        (2,'STAFF'),
+        (3,'STUDENT'),
+
+        
+    )
+
+
+
+    user_type=models.CharField(choices=USER,max_length=58,default=1)
+
+    profile.pic=models.ImageField(upload_to='media/profile_pic')
--- Hello/isbm_system/isbm_system/settings.py
+++ Hello/isbm_system/isbm_system/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [ # 3. and static file here
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL='home.CustomUser'
